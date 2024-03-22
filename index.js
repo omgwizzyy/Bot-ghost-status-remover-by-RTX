ab46ce6cad926c34e513c305fe5cdb6fe1fff6fc
omgwizzyy <164559560+omgwizzyy@users.noreply.github.com>
2024-03-22 20:35:44 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["TropicalV2","help," "Made By Omgwizzyy"];
 
 
 let currentIndex = 0;
