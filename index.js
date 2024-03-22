4220fc478b744296b6e8c5c6e2f8b21160fbad43
omgwizzyy <164559560+omgwizzyy@users.noreply.github.com>
2024-03-22 20:37:06 +0100

    index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to Tropical: http://localhost:${port}`);
+  console.log(`🔗 Powered By Tropical`);
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to TropicalV2","help"];
 
 
 let currentIndex = 0;
